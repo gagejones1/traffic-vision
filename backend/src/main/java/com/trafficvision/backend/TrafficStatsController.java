46591be27e684aa6007549df7f8b6ee171a404d5
Gage Jones <gagejones253@gmail.com>
2026-09-07 19:47:00 -0500

    Enable CORS for React frontend

--- backend/src/main/java/com/trafficvision/backend/TrafficStatsController.java
+++ backend/src/main/java/com/trafficvision/backend/TrafficStatsController.java
@@ -3,10 +3,12 @@ package com.trafficvision.backend;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/stats")
 public class TrafficStatsController {
